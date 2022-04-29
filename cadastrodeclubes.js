35863cd36d38b8ff0911d29f261e369fc046668d
Marcel-Programador <backing.audio@gmail.com>
2022-04-28 22:34:22 -0300

    Adicionado código - cadastro de clubes - Marcel de Sousa

---
+++ cadastrodeclubes.js
@@ -0,0 +1,24 @@
+var clubes = [
+    {
+      nome: "Goodbye Sunrise",
+      gestor: "R. Withfork",
+      qtdIntegrantes: 32,
+      leituraAtual: "Northern Spy",
+    },
+    {
+      nome: "Action now",
+      gestor: "Michael J.",
+      qtdIntegrantes: 30,
+      leituraAtual: "To Build a Fire",
+    },
+    {
+        nome: "Meu clube",
+        gestor: "Miguel",
+        qtdIntegrantes: 32,
+        leituraAtual: "To Build a Fire",
+    },
+  ];
+
+  for(let listar of clubes){
+      console.log(listar.nome)
+  };
