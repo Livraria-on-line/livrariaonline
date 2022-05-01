fdee5a96121aad2d4b2aeffad31a774ff6451dae
Marcel-Programador <backing.audio@gmail.com>
2022-05-01 13:34:09 -0300

    Adicionado código - Cadastro de Clubes - Marcel de Sousa

--- cadastrodeclubes.js
+++ cadastrodeclubes.js
@@ -1,24 +1,29 @@
 var clubes = [
-    {
-      nome: "Goodbye Sunrise",
-      gestor: "R. Withfork",
-      qtdIntegrantes: 32,
-      leituraAtual: "Northern Spy",
-    },
-    {
-      nome: "Action now",
-      gestor: "Michael J.",
-      qtdIntegrantes: 30,
-      leituraAtual: "To Build a Fire",
-    },
-    {
-        nome: "Meu clube",
-        gestor: "Miguel",
-        qtdIntegrantes: 32,
-        leituraAtual: "To Build a Fire",
-    },
-  ];
+  {
+    nome: "Goodbye Sunrise",
+    gestor: "R. Withfork",
+    qtdIntegrantes: 32,
+    leituraAtual: "Northern Spy",
+  },
+  {
+    nome: "Action now",
+    gestor: "Michael J.",
+    qtdIntegrantes: 30,
+    leituraAtual: "To Build a Fire",
+  },
+];
 
-  for(let listar of clubes){
-      console.log(listar.nome)
-  };
+function adicionaClube(nome, gestor, qtdIntegrantes, leituraAtual){
+  clubes.push({
+      nome: nome,
+      gestor: gestor,
+      qtdIntegrantes: qtdIntegrantes,
+      leituraAtual: leituraAtual
+  })
+};
+
+adicionaClube("Meu Clube", "Miguel", 32, "To Build a Fire")
+
+for(let listar of clubes){
+console.log(listar.nome)
+};
