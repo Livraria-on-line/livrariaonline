181e2f1b2032ebfff738a9482a336f16901da304
Victorugo238 <victor.trader238@gmail.com>
2022-05-01 22:08:10 -0300

    exercicio 4 corrigido

--- livrariaonline.js
+++ livrariaonline.js
@@ -106,17 +106,20 @@ console.log(" ")
     },
   ];
 
-  function listar(id,nome,sobreNome,idade,contato){
+  function addClientes(id,nome,sobreNome,idade,contato){
     var novosClientes = {
         id : id,
         nome : nome,
         sobreNome: sobreNome,
         idade : idade,
-        contato: contato
+        contato: contato,
       }
-      clientes.push(novosClientes)
-      
+    clientes.push(novosClientes)
+    return novosClientes
 }
+    
+addClientes(4,"victor","hugo",22,"98158-6478")
+console.log(clientes)
     
 console.log(listar(3,"victor","hugo",22,190))
 console.log(clientes)
@@ -126,31 +129,7 @@ console.log(" ")
 console.log("5- Listar clientes entre duas idades - Caio Nunez")
 console.log(" ")
 
-  var clientes = [
-    {
-      id: 1,
-      nome: "Rogério Lucas",
-      sobrenome: "Silva",
-      idade: 30,
-      contato: "4002-8922",
-    },
-    {
-      id: 2,
-      nome: "Roberto Carlos",
-      sobrenome: "Braga",
-      idade: 70,
-      contato: "(11) 9 1111-1111",
-    },
-    {
-      id: 3,
-      nome: "Sylvester",
-      sobrenome: "Stallone",
-      idade: 80,
-      contato: "(22) 9 2222-2222",
-    },
-  ];
-  
-  function filtraIdade (IdadeInicial, IdadeFinal){
+function filtraIdade (IdadeInicial, IdadeFinal){
     const clientesPorIdade = clientes.filter((clientes) => {
       if (clientes.idade >= IdadeInicial && clientes.idade <= IdadeFinal) 
       return true
